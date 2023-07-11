a03f7c82ed81aec938bbfabe5e183d96475481e9
vbnm95 <vbnm95@naver.com>
2023-07-11 23:27:08 +0900

    주차 계산 수정

--- src/DateTimeExample.java
+++ src/DateTimeExample.java
@@ -26,8 +26,10 @@ public class DateTimeExample {
 		int month = 0; // 월
 		int day = 0; // 일
 		int dw = 0; // 요일 계산을 위한 인덱스 값
+		int yy = 0; // 평년이면 0, 윤년이면 1
 		
 		int firstDayOfMonth = 0; // 당월 1일에 해당하는 요일(인덱스 값) 
+		int lastOfMonth = 0; // 월의 마지막일
 		
 		Outter: while(true) { // 1970년 이후로 평년인지 윤년인지 구분하면서 currentDay에서 월 단위로 배열에 저장된 일수를 빼서 현 시점까지 오는 계산
 			if((year%4)==0 && (year%100)!=0 || (year%400)==0) { // 윤년에 해당하는 경우
@@ -36,6 +38,7 @@ public class DateTimeExample {
 					if(currentDays <= 0) {
 						month = i+1;
 						day = (int) currentDays + leapYear[i] + 1;
+						yy = 1; // 윤년
 						break Outter;
 					}
 				}
@@ -53,11 +56,20 @@ public class DateTimeExample {
 			}
 		}
 		
-		// 주차 및 요일 계산
+		// 월의 마지막일 계산
+		if(yy == 0) { // 평년
+			lastOfMonth = normalYear[month-1];		
+		} else { // 윤년
+			lastOfMonth = leapYear[month-1];
+		}
+		
+		// 요일 계산
 		dw = (int) (currentDaysCopy % 7); // 요일 계산을 위한 인덱스 값
 		week = dayOfWeek[dw]; // 배열을 이용하여 요일 계산
+		
+		// 주차 계산
 		firstDayOfMonth = (int) (currentDaysCopy - day + 1) % 7; // 월의 첫날 요일 계산
-				
+		
 		switch(firstDayOfMonth) {
 			case 1: // 월의 첫날이 금요일
 				if(day <= 3) {
@@ -70,7 +82,7 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 1째주", month);
 				} else if(11<=day && day<=17){
 					weekLine = String.format("%02d월의 2째주", month);
-				} else if(18<=day && day<24){
+				} else if(18<=day && day<=24){
 					weekLine = String.format("%02d월의 3째주", month);
 				} else {
 					weekLine = String.format("%02d월의 4째주", month);
@@ -88,10 +100,16 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 1째주", month);
 				} else if(10<=day && day<=16){
 					weekLine = String.format("%02d월의 2째주", month);
-				} else if(17<=day && day<23){
+				} else if(17<=day && day<=23){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else {
+				} else if(24<=day && day<=30){
 					weekLine = String.format("%02d월의 4째주", month);
+				} else { // 31일이 있는 경우
+					if((month+1) == 13) {
+						weekLine = String.format("%02d월의 1째주", 1);
+					} else {
+						weekLine = String.format("%02d월의 1째주", month+1);
+					}
 				}
 				break;
 				
@@ -106,10 +124,16 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 1째주", month);
 				} else if(9<=day && day<=15){
 					weekLine = String.format("%02d월의 2째주", month);
-				} else if(16<=day && day<22){
+				} else if(16<=day && day<=22){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else {
+				} else if(23<=day && day<=29){
 					weekLine = String.format("%02d월의 4째주", month);
+				} else { // 30, 31일이 있는 경우
+					if((month+1) == 13) {
+						weekLine = String.format("%02d월의 1째주", 1);
+					} else {
+						weekLine = String.format("%02d월의 1째주", month+1);
+					}
 				}
 				break;
 			
@@ -120,10 +144,14 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 2째주", month);
 				} else if(15<=day && day<=21){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else if(22<=day && day<28){
+				} else if(22<=day && day<=28){
 					weekLine = String.format("%02d월의 4째주", month);
-				} else {
-					weekLine = String.format("%02d월의 5째주", month);
+				} else { // 29, 30, 31일이 있는 경우
+					if((month+1) == 13) {
+						weekLine = String.format("%02d월의 1째주", 1);
+					} else {
+						weekLine = String.format("%02d월의 1째주", month+1);
+					}
 				}
 				break;
 				
@@ -134,8 +162,18 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 2째주", month);
 				} else if(14<=day && day<=20){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else if(21<=day && day<27){
+				} else if(21<=day && day<=27){
 					weekLine = String.format("%02d월의 4째주", month);
+				} else if(28<=day && day<=30){
+					if(lastOfMonth == 31) {
+						weekLine = String.format("%02d월의 5째주", month);
+					} else {
+						if((month+1) == 13) {
+							weekLine = String.format("%02d월의 1째주", 1);
+						} else {
+							weekLine = String.format("%02d월의 1째주", month+1);
+						}
+					}
 				} else {
 					weekLine = String.format("%02d월의 5째주", month);
 				}
@@ -148,8 +186,18 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 2째주", month);
 				} else if(13<=day && day<=19){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else if(20<=day && day<26){
+				} else if(20<=day && day<=26){
 					weekLine = String.format("%02d월의 4째주", month);
+				} else if(27<=day && day<=29){
+					if(30 <= lastOfMonth) {
+						weekLine = String.format("%02d월의 5째주", month);
+					} else {
+						if((month+1) == 13) {
+							weekLine = String.format("%02d월의 1째주", 1);
+						} else {
+							weekLine = String.format("%02d월의 1째주", month+1);
+						}
+					}
 				} else {
 					weekLine = String.format("%02d월의 5째주", month);
 				}
@@ -162,8 +210,18 @@ public class DateTimeExample {
 					weekLine = String.format("%02d월의 2째주", month);
 				} else if(12<=day && day<=18){
 					weekLine = String.format("%02d월의 3째주", month);
-				} else if(19<=day && day<25){
+				} else if(19<=day && day<=25){
 					weekLine = String.format("%02d월의 4째주", month);
+				} else if(26<=day && day<=28){
+					if(29 <= lastOfMonth) {
+						weekLine = String.format("%02d월의 5째주", month);
+					} else {
+						if((month+1) == 13) {
+							weekLine = String.format("%02d월의 1째주", 1);
+						} else {
+							weekLine = String.format("%02d월의 1째주", month+1);
+						}
+					}
 				} else {
 					weekLine = String.format("%02d월의 5째주", month);
 				}
